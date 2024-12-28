6726661b834a196bb5b8761ac3386046d27ea4a1
Nirmal Ram <nirmalcgvfx@gmail.com>
2024-12-28 18:21:25 +0530

    Nav2 Bringup Launch
    Created a new package which has a launch file to start the nav2 bringup file with custom map and also added a node to launch rviz with specific configurations

--- turtlebot3_gazebo/launch/turtlebot3_world.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_world.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='3.0')
+    x_pose = LaunchConfiguration('x_pose', default='3.1')
     y_pose = LaunchConfiguration('y_pose', default='-3.2')
 
     world = os.path.join(
